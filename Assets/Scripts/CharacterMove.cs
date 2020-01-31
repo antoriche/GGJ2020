e4a2945c9135f825028b1b109f39cf22403f8b79
Antonin Riche <antonin@jetpack.ai>
2020-01-31 21:24:04 +0100

    character can move in the room

---
+++ Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+
+    public float speed = 1;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float vertical = Input.GetAxis ("Vertical");
+        float horizontal = Input.GetAxis ("Horizontal");
+        float height = transform.position.y;
+        transform.Translate(horizontal * speed,0,vertical * speed);
+        transform.position =  new Vector3(transform.position.x, height, transform.position.z); 
+    }
+}
