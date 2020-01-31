2e6460b6ab17f22da96d197e32b3d0ff06dc2fbb
Antonin Riche <antonin@jetpack.ai>
2020-02-01 00:56:11 +0100

    Dimension

--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -6,10 +6,11 @@ public class CharacterMove : MonoBehaviour
 {
 
     public float speed = 1;
+    Rigidbody rb;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
@@ -17,7 +18,8 @@ public class CharacterMove : MonoBehaviour
         float vertical = Input.GetAxis ("Vertical");
         float horizontal = Input.GetAxis ("Horizontal");
         float height = transform.position.y;
-        transform.Translate(horizontal * speed,0,vertical * speed);
-        transform.position =  new Vector3(transform.position.x, height, transform.position.z); 
+        
+        Vector3 newPos = new Vector3(horizontal * speed + transform.position.x, height, vertical * speed + transform.position.z); 
+        rb.MovePosition(newPos);
     }
 }
---
+++ Assets/Scripts/DimensionManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DimensionManager : Singleton<DimensionManager>
+{
+
+    public KeyCode switchKey = KeyCode.A;
+    
+    private List<DimensionObject> objects = new List<DimensionObject>();
+
+    private bool isInThePast_ = false;
+    public bool isInThePast{
+        get{
+            return isInThePast_;
+        }
+        set{
+            isInThePast_ = value;
+            foreach(DimensionObject o in objects){
+                updateObject(o);
+            }
+        }
+    }
+
+    void Update(){
+        if(Input.GetKeyDown(switchKey)){
+            isInThePast = !isInThePast;
+        }
+    }
+
+    void updateObject(DimensionObject o){
+        bool active = (o.past && isInThePast) || (o.futur && !isInThePast);
+        o.gameObject.SetActive(active);
+    }
+
+    public void register(DimensionObject o){
+        objects.Add(o);
+        updateObject(o);
+    }
+}
