3abecde95bc1b5e50bb83877ad7c3c371d5c0e1a
Antonin Riche <antonin@jetpack.ai>
2020-02-01 21:53:02 +0100

    watch

--- Assets/Scripts/DimensionManager.cs
+++ Assets/Scripts/DimensionManager.cs
@@ -4,36 +4,45 @@ using UnityEngine;
 
 public class DimensionManager : Singleton<DimensionManager>
 {
-
+    public float switchCost = 2f;
     public KeyCode switchKey = KeyCode.A;
-    
+
     private List<DimensionObject> objects = new List<DimensionObject>();
 
     private bool isInThePast_ = false;
-    public bool isInThePast{
-        get{
+    public bool isInThePast
+    {
+        get
+        {
             return isInThePast_;
         }
-        set{
+        set
+        {
             isInThePast_ = value;
-            foreach(DimensionObject o in objects){
+            foreach (DimensionObject o in objects)
+            {
                 updateObject(o);
             }
         }
     }
 
-    void Update(){
-        if(Input.GetKeyDown(switchKey)){
+    void Update()
+    {
+        if (Input.GetKeyDown(switchKey))
+        {
             isInThePast = !isInThePast;
+            Timer.Instance.timeLeft -= switchCost;
         }
     }
 
-    void updateObject(DimensionObject o){
+    void updateObject(DimensionObject o)
+    {
         bool active = (o.past && isInThePast) || (o.futur && !isInThePast);
         o.gameObject.SetActive(active);
     }
 
-    public void register(DimensionObject o){
+    public void register(DimensionObject o)
+    {
         objects.Add(o);
         updateObject(o);
     }
