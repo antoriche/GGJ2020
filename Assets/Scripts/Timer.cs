d85f96925def0052e7211f3e9029df2277d48faa
Antonin Riche <antonin@jetpack.ai>
2020-02-01 12:11:44 +0100

    hole

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : Singleton<Timer>
+{
+    public float timeLeft = 20f;
+    public float speedFactorOnPast = 2f;
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime * (DimensionManager.Instance.isInThePast ? speedFactorOnPast : 1);
+        if (timeLeft < 0)
+        {
+            Player.Instance.Die();
+        }
+    }
+}
