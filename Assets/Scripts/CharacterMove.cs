bb1bfe344482080fa5e74a1f4f28422589071b81
Antonin Riche <antonin@jetpack.ai>
2020-02-01 10:05:02 +0100

    wip

--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -7,19 +7,25 @@ public class CharacterMove : MonoBehaviour
 
     public float speed = 1;
     Rigidbody rb;
+    public GameObject player;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = player.GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
-        float vertical = Input.GetAxis ("Vertical");
-        float horizontal = Input.GetAxis ("Horizontal");
-        float height = transform.position.y;
-        
-        Vector3 newPos = new Vector3(horizontal * speed + transform.position.x, height, vertical * speed + transform.position.z); 
-        rb.MovePosition(newPos);
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        float height = player.transform.position.y;
+
+        Vector3 direction = new Vector3(horizontal * speed, 0, vertical * speed);
+        //Vector3 newPosition = rb.position + transform.TransformDirection(direction);
+        player.transform.Translate(direction * Time.deltaTime, transform);
+        player.transform.position = new Vector3(player.transform.position.x, height, player.transform.position.z);
+
+        //rb.MovePosition(player.transform.position + direction);
+        //transform.position = newPos;
     }
 }
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider))]
+public class SceneLoader : MonoBehaviour
+{
+
+    public string sceneName = null;
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "MainCamera")
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
