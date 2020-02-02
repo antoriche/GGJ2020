3677674fa808eaddc37ebf3054f248a8dd89aebd
KarineHoubrix <k.houbrix@ludus-academie.com>
2020-02-02 15:33:03 +0100

    Scène de transition + ordre des scènes

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DialogManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+    public Text dialogueText;
+
+    public Animator animator;
+
+    private void Awake()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(ObjectDialog dialogue)
+    {
+
+        animator.SetBool("IsOpen", true);
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string phrase = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(phrase));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    public void EndDialogue()
+    {
+        animator.SetBool("IsOpen", false);
+        SceneManager.LoadScene(2);
+
+    }
+}
---
+++ Assets/Scripts/DialogTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTrigger : MonoBehaviour
+{
+    public ObjectDialog dialogue;
+
+    public void Start()
+    {
+        FindObjectOfType<DialogManager>().StartDialogue(dialogue);
+    }
+
+
+}
