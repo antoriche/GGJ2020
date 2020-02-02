fc60517f26b958a9d081c48dff1513abdc745594
KarineHoubrix <k.houbrix@ludus-academie.com>
2020-02-02 10:59:27 +0100

    Finalisation du menu

---
+++ Assets/Scripts/ButtonMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonMenu : MonoBehaviour
+{
+    public Text play;
+    public Text quit;
+
+    private void OnMouseOver()
+    {
+        Debug.Log("Sur le bouton");
+        play.GetComponent<Animator>().enabled = true;
+    }
+
+    public void onClickPlay()
+    {
+        StartCoroutine(LoadSceneMenu("AntoninScene", 5f));
+        StopCoroutine("LoadSceneMenu");
+    }
+
+    public void onClickQuit()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator LoadSceneMenu(string _name, float _delay)
+    {
+        yield return new WaitForSeconds(_delay);
+        SceneManager.LoadScene(_name);
+    }
+}
