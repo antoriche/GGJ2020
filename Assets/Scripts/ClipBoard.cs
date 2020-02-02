4dadd601a0a754b0efe5f158a15832a6352287b2
Antonin Riche <antonin@jetpack.ai>
2020-02-02 16:54:08 +0100

    wip

---
+++ Assets/Scripts/ClipBoard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClipBoard : Actionnable
+{
+    // Start is called before the first frame update
+    public override void OnAction()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        SceneManager.LoadScene("Scenes/victoryEnd");
+    }
+}
---
+++ Assets/Scripts/GasMask.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GasMask : Actionnable
+{
+    public List<KillerTrap> gas;
+    public override void OnAction()
+    {
+        foreach (KillerTrap g in gas)
+        {
+            Destroy(g);
+        }
+    }
+}
